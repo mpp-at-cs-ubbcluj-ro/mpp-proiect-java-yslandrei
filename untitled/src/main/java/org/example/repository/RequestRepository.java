caa3dcf7f1431e58526861f800ce173e0a5be550
Andrei Diaconescu <andreidiaconescu2003@gmail.com>
2024-03-17 19:49:26 +0200

    update

--- untitled/src/main/java/org/example/repository/RequestRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.repository;
-
-import org.example.domain.Account;
-
-public class RequestRepository extends AbstractRepository<Account, Long>{
-    public RequestRepository(){}
-}
